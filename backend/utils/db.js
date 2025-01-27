16a4dc38975b7e93337e8d07c3ce627878bc8616
Tanmay <tanmaysinkar2021@gmail.com>
2025-01-27 21:47:09 +0530

    db

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,10 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('mongodb connected successfully');
+        await mongoose.connect(process.env.MONGO_URI, {
+            ssl: true,  // Explicitly enabling SSL
+        });
+        console.log('MongoDB connected successfully');
     } catch (error) {
-        console.log(error);
+        console.error('MongoDB connection error:', error);
     }
-}
+};
+
 export default connectDB;
